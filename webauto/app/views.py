b05711146e9483e477c389699498148f9124c830
gokulraviteja <bgokulravi1999@gmail.com>
2019-02-20 13:53:31 +0530

    committed1

---
+++ webauto/app/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from app.models import testelement
+# Create your views here.
+def home(request):
+    if(request.method== 'POST'):
+        p=request.POST['data']
+        ele=testelement.objects.get(id='1')
+        ele.value=p
+        ele.save()
+        return render(request,"app/display.html",{"ele":ele})
+    else:
+        return render(request,"app/home.html")
+
+def display(request):
+    ele=testelement.objects.get(id='1')
+    return render(request,"app/display.html",{"ele":ele})
